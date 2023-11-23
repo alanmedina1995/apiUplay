cf4dca64ee49eced9aa1e587e056a127966c8192
alanm <alanmedina1995@gmail.com>
2023-11-22 22:25:02 -0300

    Se actualizan tablas: coin y wallets

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,7 @@ CREATE TABLE IF NOT EXISTS users
     password     VARCHAR(50) not null,
     name         VARCHAR(50) not null,
     last_name    VARCHAR(50) not null,
-    phone_number VARCHAR(50) not null,
-    utn_coin     INT         not null
+    phone_number VARCHAR(50) not null
 );
 
 CREATE TABLE IF NOT EXISTS questions
@@ -26,47 +25,71 @@ CREATE TABLE IF NOT EXISTS question_Xuser
     FOREIGN KEY (question_id) REFERENCES questions (id)
 );
 
+CREATE TABLE IF NOT EXISTS coins
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    last_value  DOUBLE       NOT NULL,
+    last_update TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS wallets
+(
+    id              INT AUTO_INCREMENT PRIMARY KEY,
+    user_id         INT NOT NULL,
+    utncoin_amount  DOUBLE,
+    bitcoin_amount  DOUBLE,
+    monero_amount   DOUBLE,
+    ethereum_amount DOUBLE
+);
+
+INSERT INTO coins (description, last_value, last_update)
+VALUES ('utncoin', 5000.0, NOW()),
+       ('bitcoin', 60000.0, NOW()),
+       ('monero', 250.0, NOW()),
+       ('ethereum', 4000.0, NOW());
 
 INSERT INTO questions (question_description)
-VALUES ('¿Cuál es el nombre de tu primera mascota?'),
-       ('¿Cuál es tu comida favorita?'),
-       ('¿En qué ciudad naciste?'),
-       ('¿Cuál es el nombre de tu abuelo materno?'),
-       ('¿Cuál es tu canción favorita?'),
-       ('¿Cuál es el nombre de tu mejor amigo de la infancia?'),
-       ('¿Dónde fue tu primer trabajo?'),
-       ('¿Cuál es tu película favorita?'),
-       ('¿Cuál es el nombre de tu primer maestro?'),
-       ('¿Cuál es tu equipo deportivo favorito?'),
-       ('¿Cuál es el nombre de tu libro favorito?'),
-       ('¿Cuál es tu destino de vacaciones soñado?'),
-       ('¿Cuál es tu número de suerte?'),
-       ('¿Cuál es tu color favorito?'),
-       ('¿Cuál es tu plato típico preferido?');
+VALUES ('What is the name of your first pet?'),
+       ('What is your favorite food?'),
+       ('In which city were you born?'),
+       ('What is the name of your maternal grandfather?'),
+       ('What is your favorite song?'),
+       ('What is the name of your best childhood friend?'),
+       ('Where was your first job?'),
+       ('What is your favorite movie?'),
+       ('What is the name of your first teacher?'),
+       ('What is your favorite sports team?'),
+       ('What is the name of your favorite book?'),
+       ('What is your dream vacation destination?'),
+       ('What is your lucky number?'),
+       ('What is your favorite color?'),
+       ('What is your favorite typical dish?');
 
-INSERT INTO users (user_name, email, password, name, last_name, phone_number, utn_coin)
-VALUES ('alanmedina', 'alanmedina1995@gmail.com', 'admin1234', 'Alan', 'Medina', '2235879632', 5000),
-       ('julianzapata', 'zapatajulian79@gmail.com', 'admin1234', 'Julian', 'Zapata', '2235123487', 5000);
 
-INSERT INTO users (user_name, email, password, name, last_name, phone_number, utn_coin)
-VALUES ('luciamartinez', 'luciamartinez@gmail.com', 'admin1234', 'Lucia', 'Martinez', '2235123456', 50),
-       ('marcosrodriguez', 'marcosrodriguez@hotmail.com', 'admin1234', 'Marcos', 'Rodriguez', '2235987612', 50),
-       ('anagarcia', 'ana_garcia@yahoo.com', 'admin1234', 'Ana', 'Garcia', '2235768901', 50),
-       ('juancarloslopez', 'juancarloslopez@gmail.com', 'admin1234', 'Juan Carlos', 'Lopez', '2235874365', 50),
-       ('carolinaramirez', 'carolinaramirez@hotmail.com', 'admin1234', 'Carolina', 'Ramirez', '2235123789', 50),
-       ('pedromartinez', 'pedromartinez@gmail.com', 'admin1234', 'Pedro', 'Martinez', '2235987432', 50),
-       ('lauragonzalez', 'lauragonzalez@yahoo.com', 'admin1234', 'Laura', 'Gonzalez', '2235769087', 50),
-       ('oscarrodriguez', 'oscarrodriguez@gmail.com', 'admin1234', 'Oscar', 'Rodriguez', '2235876543', 50),
-       ('valerialopez', 'valerialopez@hotmail.com', 'admin1234', 'Valeria', 'Lopez', '2235123901', 50),
-       ('estebanperez', 'estebanperez@gmail.com', 'admin1234', 'Esteban', 'Perez', '2235987654', 50),
-       ('marianagonzalez', 'marianagonzalez@yahoo.com', 'admin1234', 'Mariana', 'Gonzalez', '2235769123', 50),
-       ('raulrodriguez', 'raulrodriguez@gmail.com', 'admin1234', 'Raul', 'Rodriguez', '2235876789', 50),
-       ('danielaramos', 'danielaramos@hotmail.com', 'admin1234', 'Daniela', 'Ramos', '2235123098', 50),
-       ('sergiohernandez', 'sergiohernandez@gmail.com', 'admin1234', 'Sergio', 'Hernandez', '2235987123', 50),
-       ('sofiatorres', 'sofiatorres@yahoo.com', 'admin1234', 'Sofia', 'Torres', '2235769456', 50),
-       ('martinromero', 'martinromero@gmail.com', 'admin1234', 'Martin', 'Romero', '2235876234', 50),
-       ('andreamartinez', 'andreamartinez@hotmail.com', 'admin1234', 'Andrea', 'Martinez', '2235123765', 50),
-       ('javierrodriguez', 'javierrodriguez@gmail.com', 'admin1234', 'Javier', 'Rodriguez', '2235987345', 50);
+INSERT INTO users (user_name, email, password, name, last_name, phone_number)
+VALUES ('alanmedina', 'alanmedina1995@gmail.com', 'admin1234', 'Alan', 'Medina', '2235879632'),
+       ('julianzapata', 'zapatajulian79@gmail.com', 'admin1234', 'Julian', 'Zapata', '2235123487');
+
+INSERT INTO users (user_name, email, password, name, last_name, phone_number)
+VALUES ('luciamartinez', 'luciamartinez@gmail.com', 'admin1234', 'Lucia', 'Martinez', '2235123456'),
+       ('marcosrodriguez', 'marcosrodriguez@hotmail.com', 'admin1234', 'Marcos', 'Rodriguez', '2235987612'),
+       ('anagarcia', 'ana_garcia@yahoo.com', 'admin1234', 'Ana', 'Garcia', '2235768901'),
+       ('juancarloslopez', 'juancarloslopez@gmail.com', 'admin1234', 'Juan Carlos', 'Lopez', '2235874365'),
+       ('carolinaramirez', 'carolinaramirez@hotmail.com', 'admin1234', 'Carolina', 'Ramirez', '2235123789'),
+       ('pedromartinez', 'pedromartinez@gmail.com', 'admin1234', 'Pedro', 'Martinez', '2235987432'),
+       ('lauragonzalez', 'lauragonzalez@yahoo.com', 'admin1234', 'Laura', 'Gonzalez', '2235769087'),
+       ('oscarrodriguez', 'oscarrodriguez@gmail.com', 'admin1234', 'Oscar', 'Rodriguez', '2235876543'),
+       ('valerialopez', 'valerialopez@hotmail.com', 'admin1234', 'Valeria', 'Lopez', '2235123901'),
+       ('estebanperez', 'estebanperez@gmail.com', 'admin1234', 'Esteban', 'Perez', '2235987654'),
+       ('marianagonzalez', 'marianagonzalez@yahoo.com', 'admin1234', 'Mariana', 'Gonzalez', '2235769123'),
+       ('raulrodriguez', 'raulrodriguez@gmail.com', 'admin1234', 'Raul', 'Rodriguez', '2235876789'),
+       ('danielaramos', 'danielaramos@hotmail.com', 'admin1234', 'Daniela', 'Ramos', '2235123098'),
+       ('sergiohernandez', 'sergiohernandez@gmail.com', 'admin1234', 'Sergio', 'Hernandez', '2235987123'),
+       ('sofiatorres', 'sofiatorres@yahoo.com', 'admin1234', 'Sofia', 'Torres', '2235769456'),
+       ('martinromero', 'martinromero@gmail.com', 'admin1234', 'Martin', 'Romero', '2235876234'),
+       ('andreamartinez', 'andreamartinez@hotmail.com', 'admin1234', 'Andrea', 'Martinez', '2235123765'),
+       ('javierrodriguez', 'javierrodriguez@gmail.com', 'admin1234', 'Javier', 'Rodriguez', '2235987345');
 
 INSERT INTO question_Xuser (user_id, question_id, answer)
 VALUES (1, 1, 'Rex'),
@@ -90,3 +113,23 @@ VALUES (1, 1, 'Rex'),
        (19, 5, 'Stairway to Heaven'),
        (20, 10, 'París');
 
+INSERT INTO wallets (user_id, utncoin_amount, bitcoin_amount, monero_amount, ethereum_amount)
+VALUES (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0),
+       (1, 50, 0, 0, 0);
